c84568236f140488a2a77c0e4534c60bf726a019
Daniel Cintra de Oliveira <cintra.daniel@icloud.com>
2021-12-08 08:38:32 -0300

    Lista de Usuários

--- PersonsAppMVC/Models/PersonsModel.cs
+++ PersonsAppMVC/Models/PersonsModel.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PersonsAppMVC.Models {
     public class PersonsModel {
 
+        [Display(Name = "ID Usuário")]
         public int UsrId { get; set; }
+
+        [Display(Name = "Data de Cadastro")]
         public DateTime UsrDtCad { get; set; }
+
+        [Display(Name = "Nome")]
         public string UsrNome { get; set; }
+
+        [Display(Name = "Sobrenome")]
         public string UsrSbnome { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
         public DateTime UsrNasci { get; set; }
+
+        [Display(Name = "Email")]
         public string UsrEmail { get; set; }
+
+        [Display(Name = "Biografia")]
         public string UsrBio { get; set; }
 
     }
